d41942edd20c2316c31b4a78a2f0c51d2b5909ce
jini0012 <jinhui.ha1208@gmail.com>
2025-04-08 12:44:46 +0900

    💄 Q&A 섹션의 결과 계산 로직 추가 및 선택한 답변 저장 기능 구현

--- js/start.js
+++ js/start.js
@@ -1,6 +1,45 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 const result = document.querySelector("#result");
+const selectList = [];
+
+function calcResult() {
+  const pointArray = [
+    { name: "mouse", value: 0, key: 0 },
+    { name: "cow", value: 0, key: 1 },
+    { name: "tiger", value: 0, key: 2 },
+    { name: "rabbit", value: 0, key: 3 },
+    { name: "dragon", value: 0, key: 4 },
+    { name: "snake", value: 0, key: 5 },
+    { name: "horse", value: 0, key: 6 },
+    { name: "sheep", value: 0, key: 7 },
+    { name: "monkey", value: 0, key: 8 },
+    { name: "chick", value: 0, key: 9 },
+    { name: "dog", value: 0, key: 10 },
+    { name: "pig", value: 0, key: 11 },
+  ];
+
+  for (let i = 0; i < selectList.length; i++) {
+    const target = qnaList[i].a[selectList[i]].type;
+    for (let j = 0; j < target.length; j++) {
+      for (let k = 0; k < pointArray.length; k++) {
+        if (target[j] === pointArray[k].name) {
+          pointArray[k].value++;
+        }
+      }
+    }
+  }
+
+  const resultArray = pointArray.sort((a, b) => {
+    if (a.value > b.value) return -1;
+    if (a.value < b.value) return 1;
+    return 0;
+  });
+
+  let resultType = resultArray[0].key;
+
+  return resultType;
+}
 
 function goResult() {
   qna.style.WebkitAnimation = "fadeOut 1s";
@@ -13,9 +52,10 @@ function goResult() {
       result.style.display = "block";
     }, 450);
   });
+  calcResult();
 }
 
-function addAnswer(answerText, qIndex) {
+function addAnswer(answerText, qIndex, aIndex) {
   const a = document.querySelector(".answerBox");
   const answer = document.createElement("button");
   answer.classList.add("answerList");
@@ -25,6 +65,7 @@ function addAnswer(answerText, qIndex) {
   a.appendChild(answer);
   answer.textContent = answerText;
 
+  // 사용자의 answer 버튼 선택 이벤트 처리 로직
   answer.addEventListener(
     "click",
     function () {
@@ -36,6 +77,7 @@ function addAnswer(answerText, qIndex) {
         children[i].style.animation = "fadeOut 0.5s";
       }
       setTimeout(() => {
+        selectList[qIndex] = aIndex; // 사용자가 어떤 버튼에서 어떤 답변을 선택했는지 저장
         for (let i = 0; i < children.length; i++) {
           children[i].style.display = "none";
         }
@@ -58,12 +100,11 @@ function goNext(qIndex) {
   const a = document.querySelector(".answerBox");
 
   for (let i in qnaList[qIndex].a) {
-    addAnswer(qnaList[qIndex].a[i].answer, qIndex);
+    addAnswer(qnaList[qIndex].a[i].answer, qIndex, i);
   }
 
   const progress = document.querySelector(".progress");
   const progressBar = document.querySelector(".progress-bar");
-
   progressBar.style.width = (100 / qnaList.length) * (qIndex + 1) + "%";
   progress.setAttribute(
     "aria-valuenow",

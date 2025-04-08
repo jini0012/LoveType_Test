b3fdcd66a7d42d04e0b48b5c17aa9be8db8873bc
jini0012 <jinhui.ha1208@gmail.com>
2025-04-08 11:25:14 +0900

    💄 Q&A 섹션에 답변 버튼 및 data.js 적용

--- js/start.js
+++ js/start.js
@@ -1,6 +1,34 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 
+function addAnswer(answerText, qIndex) {
+  const a = document.querySelector(".answerBox");
+  const answer = document.createElement("button");
+  answer.classList.add("answerList");
+  a.appendChild(answer);
+  answer.textContent = answerText;
+
+  answer.addEventListener("click", function () {
+    const children = document.querySelectorAll(".answerList");
+
+    for (let i = 0; i < children.length; i++) {
+      children[i].disabled = true;
+      children[i].style.display = "none";
+    }
+    goNext(qIndex + 1);
+  });
+}
+
+function goNext(qIndex) {
+  const q = document.querySelector(".qBox");
+  q.innerHTML = qnaList[qIndex].q;
+  const a = document.querySelector(".answerBox");
+
+  for (let i in qnaList[qIndex].a) {
+    addAnswer(qnaList[qIndex].a[i].answer, qIndex);
+  }
+}
+
 function start() {
   main.style.WebkitAnimation = "fadeOut 1s";
   main.style.animation = "fadeOut 1s";
@@ -11,5 +39,7 @@ function start() {
       main.style.display = "none";
       qna.style.display = "block";
     }, 450);
+    let qIndex = 0;
+    goNext(qIndex);
   }, 450);
 }
